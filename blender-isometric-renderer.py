1992f25c40c450c108469db0f8be2e76a3bc963b
Chen Chin Jieh <chinjieh@gmail.com>
2021-06-03 22:40:27 +0800

    Fix var name not matching

--- blender-isometric-renderer.py
+++ blender-isometric-renderer.py
@@ -147,9 +147,9 @@ def main():
 	else:
 		output_path = os.path.abspath(args.output_path)
 
-	if os.path.exists(output_path) and os.path.samefile(input_path, output_path):
+	if os.path.exists(output_path) and os.path.samefile(input_obj_path, output_path):
 		print("Error: The input path and output paths are the same, aborting to prevent overwriting source work.")
-		print("Input: %s" % input_path)
+		print("Input: %s" % input_obj_path)
 		return
 
 	import_settings = build_import_settings(args)
