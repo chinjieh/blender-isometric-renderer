8f6bf60e1ccfa46e7d99b78af30d7e51cca29230
Chen Chin Jieh <chinjieh@gmail.com>
2021-06-03 18:43:23 +0800

    Cleanup code

--- blender-isometric-renderer.py
+++ blender-isometric-renderer.py
@@ -3,11 +3,12 @@
 # it will import the given obj file, and render the object in eight 45 degrees angles.
 # Renders are exported to the given render path, with the direction suffix (e.g "-SE")
 
-import bpy
 import os
 import math
 
 def import_and_export_obj(path_to_obj, render_path_root):
+	import bpy
+
 	# import file
 	bpy.ops.object.select_all(action='DESELECT')
 	filepath = path_to_obj
@@ -19,10 +20,9 @@ def import_and_export_obj(path_to_obj, render_path_root):
 		dirs = ["SE", "S", "SW", "W", "NW", "N", "NE", "E"]
 		original_output_path = bpy.data.scenes["Scene"].render.filepath
 		output_filename_base = os.path.splitext(os.path.basename(filepath))[0]
-		output_path_base = render_path_root
 		for dir in dirs:
 			output_filename = output_filename_base + "-" + dir
-			output_path = os.path.join(output_path_base, output_filename)
+			output_path = os.path.join(render_path_root, output_filename)
 			bpy.data.scenes["Scene"].render.filepath = output_path
 			print("Exporting to " + output_path)
 			bpy.ops.render.render(write_still=True)
@@ -36,40 +36,41 @@ def import_and_export_obj(path_to_obj, render_path_root):
 		imported_obj.select_set(True)
 		bpy.ops.object.delete()
 
+
 def build_render_path_root_matching_structure(root_folder, file_path_in_root_folder, export_folder):
 	# Builds a render path as a folder with the same relative path as the file
 	relative = os.path.relpath(file_path_in_root_folder, root_folder)
-	relative = os.path.splitext(relative)[0] # Gets the relative structure without the file ext
+	relative = os.path.splitext(relative)[0] # Gets the relative structure without the file extension
 	return os.path.join(export_folder, relative)
+
 
 def is_obj_file(path_to_obj):
 	return os.path.splitext(path_to_obj)[1] == ".obj"
 
-def import_and_export(path_to_obj, render_path_root, is_folder_mode=False):
+
+def import_and_export(input_path, render_path_root, is_folder_mode=False):
 	if not is_folder_mode:
-		if not is_obj_file(path_to_obj):
+		if not is_obj_file(input_path):
 			print("Error: Obj path provided is not a valid .obj file")
 			return;
 
-		import_and_export_obj(path_to_obj, render_path_root)
+		import_and_export_obj(input_path, render_path_root)
 		return;
 
 	else:
-		print("Finding files in folder: " + path_to_obj)
-		if not os.path.isdir(path_to_obj):
-			print("Error: Folder mode used, obj path is supposed to be a directory.")
+		print("Finding files in folder: " + input_path)
+		if not os.path.isdir(input_path):
+			print("Error: Folder mode is active, so the input path is supposed to be a directory.")
 			return;
-		root_path = path_to_obj
-		export_root_folder = render_path_root
 
-		for (dirpath, dirnames, filenames) in os.walk(root_path):
+		for (dirpath, dirnames, filenames) in os.walk(input_path):
 			for filename in filenames:
 				if is_obj_file(filename):
 					obj_path = os.path.join(dirpath, filename)
 					if (os.path.isfile(obj_path)):
 						print("Found obj file: " + obj_path)
-						render_path_root = build_render_path_root_matching_structure(root_path, obj_path, export_root_folder)
-						import_and_export_obj(obj_path, render_path_root)
+						export_root_folder = build_render_path_root_matching_structure(input_path, obj_path, render_path_root)
+						import_and_export_obj(obj_path, export_root_folder)
 		
 
 def main():
@@ -88,28 +89,33 @@ def main():
 	# When --help or no args are given, print this help
 	usage_text = (
 		"Run blender in background mode with this script:"
-		"  blender --background --python " + __file__ + " -- [options]"
+		"  blender --background <path_to_blend_file> --python " + __file__ + " -- [options]"
 		)
 
 	parser = argparse.ArgumentParser(description=usage_text)
-	parser.add_argument("-i", "--input", dest="input_obj", type=str, required=True, help="Path to the obj file OR folder (with --foldermode enabled)",)
-	parser.add_argument("-o", "--output", dest="output_path", metavar='PATH', help="Save the generated renders to the specified folder. If absent, defaults to an 'exported' folder as a sibling of the input path",)
-	parser.add_argument("-f", "--foldermode", default=False, dest="foldermode", action='store_true', help="Folder mode; Assumes the input path is a folder containing obj files")
+	parser.add_argument("-i", "--input", dest="input_path", type=str, required=True, 
+		help="Path to the obj file OR folder (with --foldermode enabled)",)
+
+	parser.add_argument("-o", "--output", dest="output_path", metavar='PATH', 
+		help="Save the generated renders to the specified folder. If absent, defaults to an 'export' folder as a sibling of the input path",)
+
+	parser.add_argument("-f", "--foldermode", default=False, dest="foldermode", action='store_true', 
+		help="Folder mode; Assumes the input path is a folder containing obj files",)
 	
 	args = parser.parse_args(argv)
 	if not argv:
 		parser.print_help()
 		return
 
-	if not args.input_obj:
-		print("Error: --i=\"some string\" argument not given, aborting.")
+	if not args.input_path:
+		print("Error: --i=\"input_path\" argument not given, aborting.")
 		parser.print_help()
 		return
 
-	input_obj_path = os.path.abspath(args.input_obj)
+	input_obj_path = os.path.abspath(args.input_path)
 
 	if not args.output_path:
-		output_path = os.path.join(os.path.split(input_obj_path)[0], "exported")
+		output_path = os.path.join(os.path.split(input_obj_path)[0], "export")
 	else:
 		output_path = os.path.abspath(args.output_path)
 
